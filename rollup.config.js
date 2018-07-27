f5e0c95e2d79ecce208b6a00cb29a95a61c5ddc1
Malash <i@malash.me>
2018-07-27 17:33:14 +0800

    remove useless umd bundle

--- rollup.config.js
+++ rollup.config.js
@@ -1,14 +1,10 @@
-import resolve from 'rollup-plugin-node-resolve';
 import commonjs from 'rollup-plugin-commonjs';
 import babel from 'rollup-plugin-babel';
-import replace from 'rollup-plugin-replace';
-import uglify from 'rollup-plugin-uglify';
 import filesize from 'rollup-plugin-filesize';
 import { name, version, dependencies, devDependencies, peerDependencies } from './package.json';
 
 const target = process.env.TARGET || 'es';
 const env = process.env.NODE_ENV || 'development';
-const isProd = env === 'production';
 const banner = `/*
  * @license
  * ${name} v${version}
@@ -35,30 +31,10 @@ const config = {
   ],
   output: {
     banner,
-    file: `dist/${name}.${target}.${isProd ? 'min.js' : 'js'}`,
+    file: `dist/${name}.${target}.js`,
     name: 'MpSvgLoader',
     format: target,
   },
 };
-
-if (target === 'umd') {
-  config.external = Object.keys(Object.assign({}, peerDependencies));
-  config.plugins = [].concat(
-    [resolve()],
-    config.plugins,
-    [replace({ 'process.env.NODE_ENV': JSON.stringify(process.env.NODE_ENV) })],
-  );
-}
-
-if (isProd) {
-  config.plugins = [].concat(
-    config.plugins,
-    [uglify({
-      output: {
-        comments: (node, comment) => comment.type === 'comment2' && /@preserve|@license|@cc_on/i.test(comment.value),
-      },
-    })],
-  );
-}
 
 export default config;
